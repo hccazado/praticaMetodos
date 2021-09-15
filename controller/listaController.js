0277add380187cc3cb59eca0c37176533e0a0f7a
hccazado <heitorcazado@gmail.com>
2021-09-15 10:53:08 -0300

    adicionando multer e input para arquivo em cadastro

--- controller/listaController.js
+++ controller/listaController.js
@@ -1,5 +1,19 @@
 const title = "Listagem - trabalhando verbos HTTP";
 const listaModel = require("../model/listaModel");
+const multer = require('multer');
+
+//Definindo destino para o multer
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null,'public/img/uploads')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname+'-'+Date.now())
+    }
+})
+
+//instanciando middleware do multer com a definição do storage
+const upload = multer({storage: storage});
 
 const controller = {
     home: (req,res,next) =>{
@@ -15,6 +29,7 @@ const controller = {
     },
     criarItem: (req, res, next) =>{
         let {id, nome} = req.body;
+        console.log(req.file);
         listaModel.addItem(id, nome);
         res.redirect("/");
     },
--- routes/lista.js
+++ routes/lista.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controller/listaController");
+const multer = require('multer');
+
+//Definindo destino para o multer
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null,'public/images/uploads')
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname+'-'+Date.now())
+    }
+})
+
+//instanciando middleware do multer com a definição do storage
+const upload = multer({storage: storage});
 
 router.get("/", controller.home);
 router.get("/adicionar", controller.adiconarForm);
-router.post("/adicionar", controller.criarItem);
+router.post("/adicionar", upload.single("avatarFile"), controller.criarItem);
 router.get("/:id/editar", controller.editarForm);
 router.put("/:id/editar", controller.editarItem);
 router.delete("/:id/excluir", controller.deleteItem);
